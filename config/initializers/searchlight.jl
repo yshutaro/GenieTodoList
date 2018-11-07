f06b2d9d7ed567b1147156e18daeed6704f013f5
yshutaro <y.shutaro@gmail.com>
2018-11-08 00:49:29 +0900

    起動時のSearchLight読み込み

--- config/initializers/searchlight.jl
+++ config/initializers/searchlight.jl
@@ -1,6 +1,5 @@
 # Uncomment the code to enable SearchLight support
 
-#=
 using SearchLight, SearchLight.QueryBuilder
 
 Core.eval(SearchLight, :(config.db_config_settings = SearchLight.Configuration.load_db_connection()))
@@ -18,4 +17,3 @@ Core.eval(Genie.Generator, :(using SearchLight, SearchLight.Migration))
 Core.eval(Genie.Tester, :(using SearchLight, SearchLight.Migration))
 Core.eval(Genie.Commands, :(using SearchLight, SearchLight.Migration))
 Core.eval(Genie.REPL, :(using SearchLight, SearchLight.Generator, SearchLight.Migration))
-=#
